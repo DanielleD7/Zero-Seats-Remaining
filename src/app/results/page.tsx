75b6e135dc9463347bbd400c4896e015d48c47a2
Nitrorex17 <115041681+Nitrorex17@users.noreply.github.com>
2024-10-31 14:34:10 -0400

    THE LAST ONEEE PLEASE

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useSearchParams } from 'react'
+import { useState } from 'react'
 import { ChevronLeft, ShoppingCart, ChevronDown, Plus } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Course, Section, Courses } from "@/components/ui/data"
